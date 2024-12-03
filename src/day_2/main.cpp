9926f794a6cd306040c1f2dad919b28357aaab3c
jsanestrzik <j.sanestrzik@gmail.com>
2024-12-03 11:59:17 +0100

    Restructure code

--- src/day_2/main.cpp
+++ src/day_2/main.cpp
@@ -17,32 +17,6 @@ void read_input_from_file(std::ifstream &f_stream, Func func) {
     }
 }
 
-long find_total_distance(std::vector<long> lhs, std::vector<long> rhs) {
-    assert(lhs.size() == rhs.size());
-
-    std::vector<long> result(lhs.size());
-
-    std::transform(
-        lhs.begin(), 
-        lhs.end(), 
-        rhs.begin(),
-        result.begin(), 
-        [](long lv, long rv) { return std::abs(lv - rv); }
-    );
-
-    return std::accumulate(result.begin(), result.end(), 0);
-}
-
-long calculate_similarity_score(std::vector<long> &lhs, std::vector<long> &rhs) {
-    long score = 0;
-    
-    for (long location_id : lhs) {
-        score += location_id * std::count(rhs.begin(), rhs.end(), location_id);
-    }
-
-    return score;
-}
-
 bool is_monotonic(std::vector<long> &vec) {
     bool asc = true, desc = true;
 
@@ -97,28 +71,7 @@ long calculate_valid_reports(std::vector<std::vector<long>> &reports) {
 int main() {
     std::cout << "Advent of code 2024" << std::endl;
 
-    std::ifstream Input("../resources/input_day1_1.txt");
-    
-    std::vector<long> left_list;
-    std::vector<long> right_list;
-
-    read_input_from_file(Input, [&](std::string line) {
-        size_t pos = 0;
-        const std::string delim("   ");
-        while((pos = line.find(delim)) != std::string::npos) {
-            left_list.emplace_back(std::stol(line.substr(0, pos)));
-            line.erase(line.begin(), line.begin() + pos + delim.size());
-            right_list.emplace_back(std::stol(line));
-        }
-    });
-
-    std::sort(left_list.begin(), left_list.end());
-    std::sort(right_list.begin(), right_list.end());
-
-    long distance = find_total_distance(left_list, right_list);
-    long score = calculate_similarity_score(left_list, right_list);
-
-    std::ifstream Reposts("../resources/input_day2_1.txt");
+    std::ifstream Reposts("../../resources/input_day2_1.txt");
     std::vector<std::vector<long>> reports;
 
     read_input_from_file(Reposts, [&](std::string line) {
@@ -133,18 +86,8 @@ int main() {
         reports.emplace_back(levels);
     });
     
-//    std::vector<std::vector<long>> reports{
-//        std::vector<long>{7, 6, 4, 2, 1},
-//        std::vector<long>{1, 2, 7, 8, 9},
-//        std::vector<long>{9, 7, 6, 2, 1},
-//        std::vector<long>{1, 3, 2, 4, 5},
-//        std::vector<long>{8, 6, 4, 4, 1},
-//        std::vector<long>{1, 3, 6, 7, 9},
-//    };
     long valid_reports = calculate_valid_reports(reports);
 
-    std::cout << "Found distance: " << distance << std::endl;
-    std::cout << "Similarity score: " << score << std::endl;
     std::cout << "Valid report: " << valid_reports << std::endl;
 
     Input.close();
